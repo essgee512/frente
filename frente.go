0019782f6145e4db247edc5dd0ec59f228da2d53
essgee512 <essgee512@gmail.com>
2019-03-20 22:00:06 -0500

    gofmt

--- frente.go
+++ frente.go
@@ -1,94 +1,94 @@
 package frente
 
 import (
-  "bufio"
-  "bytes"
-  "fmt"
-  "io"
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
 )
 
 const (
-  eof   = ""    // peek returns empty string at EOF
-  nl    = "\n"
+	eof = "" // peek returns empty string at EOF
+	nl  = "\n"
 )
 
 func Split(matter []byte, delim string) ([]byte, []byte, error) {
-  p := newParser(bufio.NewReader(bytes.NewBuffer(matter)), delim)
-  openDelim := delim + nl
-  closeDelim := nl + delim + nl
-  empty := []byte("")
-  front := bytes.NewBufferString("")
-  body := bytes.NewBufferString("")
+	p := newParser(bufio.NewReader(bytes.NewBuffer(matter)), delim)
+	openDelim := delim + nl
+	closeDelim := nl + delim + nl
+	empty := []byte("")
+	front := bytes.NewBufferString("")
+	body := bytes.NewBufferString("")
 
-  // matter must begin with an opening delimeter
-  if !p.isNext(openDelim) {
-    return empty, empty, fmt.Errorf("matter must begin with ODL := '%s[NL]'.", delim)
-  }
+	// matter must begin with an opening delimeter
+	if !p.isNext(openDelim) {
+		return empty, empty, fmt.Errorf("matter must begin with ODL := '%s[NL]'.", delim)
+	}
 
-  p.readN(len(openDelim))                     // consume opening delimiter
-  f, err := p.readUpto(closeDelim)            // capture front
-  front.WriteString(f)                        // write front
+	p.readN(len(openDelim))          // consume opening delimiter
+	f, err := p.readUpto(closeDelim) // capture front
+	front.WriteString(f)             // write front
 
-  // matter must have a closing delimeter
-  if err != nil {
-    return empty, empty, fmt.Errorf("matter must have a CDL := '[NL]%s[NL]'.", delim)
-  }
+	// matter must have a closing delimeter
+	if err != nil {
+		return empty, empty, fmt.Errorf("matter must have a CDL := '[NL]%s[NL]'.", delim)
+	}
 
-  p.readN(len(closeDelim))                    // consume closing delimeter
-  b, _ := p.readUpto(eof)                     // capture body
-  body.WriteString(b)                         // write body
+	p.readN(len(closeDelim)) // consume closing delimeter
+	b, _ := p.readUpto(eof)  // capture body
+	body.WriteString(b)      // write body
 
-  return front.Bytes(), body.Bytes(), nil     // success
+	return front.Bytes(), body.Bytes(), nil // success
 }
 
 type parser struct {
-  r *bufio.Reader
+	r *bufio.Reader
 }
 
 func newParser(r io.Reader, delim string) *parser {
-  return &parser{bufio.NewReader(r)}
+	return &parser{bufio.NewReader(r)}
 }
 
 func (p *parser) isNext(str string) bool {
-  n := len(str)
-  if str == eof {
-    n = 1
-  }
-  peek, _ := p.r.Peek(n)
-  if string(peek) == str {
-    return true
-  }
-  return false
+	n := len(str)
+	if str == eof {
+		n = 1
+	}
+	peek, _ := p.r.Peek(n)
+	if string(peek) == str {
+		return true
+	}
+	return false
 }
 
 func (p *parser) readN(n int) string {
-  var buf bytes.Buffer
-  for i := 0; i < n; i++ {
-    buf.WriteRune(p.read())
-  }
-  return buf.String()
+	var buf bytes.Buffer
+	for i := 0; i < n; i++ {
+		buf.WriteRune(p.read())
+	}
+	return buf.String()
 }
 
 func (p *parser) readUpto(str string) (string, error) {
-  var buf bytes.Buffer
-  if p.isNext(eof) {
-    return eof, fmt.Errorf("%q not found", str)
-  }
-  buf.WriteRune(p.read())
-  for {
-    if p.isNext(eof) {
-      break
-    } else if p.isNext(str) {
-      return buf.String(), nil
-    }
-    buf.WriteRune(p.read())
-  }
-  return buf.String(), fmt.Errorf("%q not found", str)
+	var buf bytes.Buffer
+	if p.isNext(eof) {
+		return eof, fmt.Errorf("%q not found", str)
+	}
+	buf.WriteRune(p.read())
+	for {
+		if p.isNext(eof) {
+			break
+		} else if p.isNext(str) {
+			return buf.String(), nil
+		}
+		buf.WriteRune(p.read())
+	}
+	return buf.String(), fmt.Errorf("%q not found", str)
 }
 
 func (p *parser) read() rune {
-  ch, _, _ := p.r.ReadRune()
-  return ch
+	ch, _, _ := p.r.ReadRune()
+	return ch
 }
 
 func (p *parser) unread() { _ = p.r.UnreadRune() }
--- frente_test.go
+++ frente_test.go
@@ -1,94 +1,84 @@
 package frente_test
 
 import (
-  "fmt"
-  "reflect"
-  "testing"
+	"fmt"
+	"reflect"
+	"testing"
 
-  "github.com/essgee512/frente"
+	"github.com/essgee512/frente"
 )
 
 func TestSplit(t *testing.T) {
-  const delim string = "---"
-  var scenarios = []struct {
-    in    []byte
-    front []byte
-    body  []byte
-    err   error
-  }{
-    {
-      // nominal case
-      in:    []byte("---\nfront\n---\nbody"),
-      front: []byte("front"),
-      body:  []byte("body"),
-      err:   nil,
-    },
-    {
-      // body has CDL
-      in:    []byte("---\nfront\n---\nbody\n---\n"),
-      front: []byte("front"),
-      body:  []byte("body\n---\n"),
-      err:   nil,
-    },
-    {
-      // body has CDL edge case
-      in:    []byte("---\nfront\n---\n---\n"),
-      front: []byte("front"),
-      body:  []byte("---\n"),
-      err:   nil,
-    },
-    {
-      in:    []byte("  ---\nfront\n---\nbody"),
-      front: []byte(""),
-      body:  []byte(""),
-      err:   fmt.Errorf("matter must begin with ODL := '---[NL]'."),
-    },
-    {
-      in:    []byte("---  \nfront\n---\nbody"),
-      front: []byte(""),
-      body:  []byte(""),
-      err:   fmt.Errorf("matter must begin with ODL := '---[NL]'."),
-    },
-    {
-      in:    []byte("---\nfront\nmissing CDL\n"),
-      front: []byte(""),
-      body:  []byte(""),
-      err:   fmt.Errorf("matter must have a CDL := '[NL]---[NL]'."),
-    },
-    {
-      in:    []byte("---\nfront\n ---\nbody\n"),
-      front: []byte(""),
-      body:  []byte(""),
-      err:   fmt.Errorf("matter must have a CDL := '[NL]---[NL]'."),
-    },
-    {
-      in:    []byte("---\nfront\n--- \nbody\n"),
-      front: []byte(""),
-      body:  []byte(""),
-      err:   fmt.Errorf("matter must have a CDL := '[NL]---[NL]'."),
-    },
-  }
+	const delim string = "---"
+	var scenarios = []struct {
+		in    []byte
+		front []byte
+		body  []byte
+		err   error
+	}{
+		{
+			// nominal case
+			in:    []byte("---\nfront\n---\nbody"),
+			front: []byte("front"),
+			body:  []byte("body"),
+			err:   nil,
+		},
+		{
+			// body has CDL
+			in:    []byte("---\nfront\n---\nbody\n---\n"),
+			front: []byte("front"),
+			body:  []byte("body\n---\n"),
+			err:   nil,
+		},
+		{
+			// body has CDL edge case
+			in:    []byte("---\nfront\n---\n---\n"),
+			front: []byte("front"),
+			body:  []byte("---\n"),
+			err:   nil,
+		},
+		{
+			in:    []byte("  ---\nfront\n---\nbody"),
+			front: []byte(""),
+			body:  []byte(""),
+			err:   fmt.Errorf("matter must begin with ODL := '---[NL]'."),
+		},
+		{
+			in:    []byte("---  \nfront\n---\nbody"),
+			front: []byte(""),
+			body:  []byte(""),
+			err:   fmt.Errorf("matter must begin with ODL := '---[NL]'."),
+		},
+		{
+			in:    []byte("---\nfront\nmissing CDL\n"),
+			front: []byte(""),
+			body:  []byte(""),
+			err:   fmt.Errorf("matter must have a CDL := '[NL]---[NL]'."),
+		},
+		{
+			in:    []byte("---\nfront\n ---\nbody\n"),
+			front: []byte(""),
+			body:  []byte(""),
+			err:   fmt.Errorf("matter must have a CDL := '[NL]---[NL]'."),
+		},
+		{
+			in:    []byte("---\nfront\n--- \nbody\n"),
+			front: []byte(""),
+			body:  []byte(""),
+			err:   fmt.Errorf("matter must have a CDL := '[NL]---[NL]'."),
+		},
+	}
 
-  for i, expected := range scenarios {
-    f, b, err := frente.Split(expected.in, delim)
-    if !reflect.DeepEqual(f, expected.front) {
-      t.Errorf("sc %d. front mismatch:\n  expected %q\n  got      %q", i+1, expected.front, f)
-    }
-    if !reflect.DeepEqual(b, expected.body) {
-      t.Errorf("sc %d. body  mismatch:\n  expected %q\n  got      %q", i+1, expected.body, b)
-    }
-    if !reflect.DeepEqual(err, expected.err) {
-      t.Errorf("sc %d. error mismatch:\n  expected %q\n  got      %q", i+1, expected.err, err)
-    }
-  }
+	for i, expected := range scenarios {
+		f, b, err := frente.Split(expected.in, delim)
+		if !reflect.DeepEqual(f, expected.front) {
+			t.Errorf("sc %d. front mismatch:\n  expected %q\n  got      %q", i+1, expected.front, f)
+		}
+		if !reflect.DeepEqual(b, expected.body) {
+			t.Errorf("sc %d. body  mismatch:\n  expected %q\n  got      %q", i+1, expected.body, b)
+		}
+		if !reflect.DeepEqual(err, expected.err) {
+			t.Errorf("sc %d. error mismatch:\n  expected %q\n  got      %q", i+1, expected.err, err)
+		}
+	}
 }
-
-
-
-
-
-
-
-
-
-
